986391e6dc689d7f292fee5ffcabbe81693058ab
swhwang <83943446+swhwang81@users.noreply.github.com>
2023-06-02 12:15:11 +0900

    Add files via upload

--- home.py
+++ home.py
@@ -1,29 +1,16 @@
 import streamlit as st 
 
-st.write('# Hi! Welcome to My App!')
+st.write('# 체질량 지수 계산기')
 
-st.write('Nice to meet you!')
+height = st.number_input('신장 (cm)',value = 160, step =5)
+st.write(height,'cm')
 
+weight = st.number_input('체중 (kg)', value = 50, step =5)
+st.write(weight,'kg')
 
-if st.button('Say hello'):
-    st.write('Why hello there')
-else:
-    st.write('Goodbye')
+bmi = weight/((height/100)**2)
+
+if st.button('계산'):
+    st.write('당신의 체질량 지수는', round(bmi,2), '입니다.')
     
-    
-option = st.selectbox(
-    '좋아하는 동물은?',
-    ('강아지', '고양이', '말', '토끼', '코끼리'))
 
-st.write('내가 좋아하는 동물은', option, '입니다.')
-st.write(f'좋아하는 동물은 {option} 입니다.')
-
-
-txt = st.text_area('자신을 소개해보세요.', '''
-    
-    ''')
-
-st.write('입력한 내용은: ', txt)
-
-age = st.slider('나이를 선택하세요.', 0, 130, 25)
-st.write("저의 나이는 ", age, '입니다.')
